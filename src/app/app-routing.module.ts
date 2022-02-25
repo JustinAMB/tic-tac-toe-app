4c7f007e8eb38731a59134e3c15d2ac0d4ae860d
JustinAMB <Justinmb@outlook.es>
2022-02-25 15:44:44 -0600

    se definen las rutas del modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresarFormComponent } from './components/ingresar-form/ingresar-form.component';
+import { SalaEsperaComponent } from './components/sala-espera/sala-espera.component';
+import { TableroComponent } from './components/tablero/tablero.component';
 
 const routes: Routes = [
-  { path: 'juego', redirectTo: 'tablero'},
+ 
+  {
+    path:'',
+    component:IngresarFormComponent
+  },{
+    path:'sala',
+    component:SalaEsperaComponent
+
+  
+  },{
+    path:'ingresar/:sala',
+    component:IngresarFormComponent
+  },{
+    path:'partida/:sala',
+    component:TableroComponent
+  }
+
 ];
 
 @NgModule({
