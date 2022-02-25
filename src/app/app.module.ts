61e1c9ea6282aa8a4f91821909ceec405c38fe04
JustinAMB <Justinmb@outlook.es>
2022-02-25 15:46:20 -0600

    cambios generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,20 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableroComponent } from './components/tablero/tablero.component';
+import { IngresarFormComponent } from './components/ingresar-form/ingresar-form.component';
+import { FormsModule } from '@angular/forms';
+import { SalaEsperaComponent } from './components/sala-espera/sala-espera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableroComponent
+    TableroComponent,
+    IngresarFormComponent,
+    SalaEsperaComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     SocketIoModule.forRoot( environment.socketConfig ),
     AppRoutingModule
   ],
