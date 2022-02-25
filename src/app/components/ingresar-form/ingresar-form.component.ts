412c29bae3e0c88cc19e6f684acbb09b2531c1bb
JustinAMB <Justinmb@outlook.es>
2022-02-24 23:47:07 -0600

    se crea la logica del componente

--- src/app/components/ingresar-form/ingresar-form.component.ts
+++ src/app/components/ingresar-form/ingresar-form.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PartidaService } from 'src/app/services/partida.service';
+import { WebsocketService } from 'src/app/services/websocket.service';
+import { v4 as uuidv4 } from 'uuid';
 @Component({
   selector: 'app-ingresar-form',
   templateUrl: './ingresar-form.component.html',
   styleUrls: ['./ingresar-form.component.css']
 })
 export class IngresarFormComponent implements OnInit {
-
-  constructor() { }
+  name:string='';
+  constructor(private webSocket:WebsocketService,private partidaService:PartidaService) { }
 
   ngOnInit(): void {
+  }
+
+  ingresar() {
+    const sala = uuidv4();
+    const data={
+      sala,
+      name:this.name
+    }
+    this.partidaService.sala=sala;
+    this.webSocket.emit('ingresar',data); //emitir evento al servidor
+
+
+
   }
 
 }
