414d6d2eacfffb9193a6f8beb67d3fde9d33193a
JustinAMB <Justinmb@outlook.es>
2022-03-05 14:12:50 -0600

    se agrega las combinaciones para que alguien gane

--- src/app/services/partida.service.ts
+++ src/app/services/partida.service.ts
@@ -8,8 +8,20 @@ export class PartidaService {
   sala:string=this.salaLocal();
   jugadores:Jugador[]=[];
   created:boolean=false;
+  private _winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
   constructor() { }
-
+  get winningCombinations():number[][]{
+    return [...this._winningCombinations];
+  }
 
 
   salaLocal():string{
