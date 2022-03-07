6536e1a4479a18327dcc820054978bf9024514b1
JustinAMB <Justinmb@outlook.es>
2022-03-06 20:31:23 -0600

    cambios para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+import { SocketIoConfig } from "ngx-socket-io";
+
+const config: SocketIoConfig = { url: 'https://tic-tac-toe-j.herokuapp.com/', options: {
+  transports: ['websocket']
+} };
+
 export const environment = {
-  production: true
+  production: true,
+  socketConfig:config
 };
